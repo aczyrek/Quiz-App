a202ee05f99e3517d4cdbde5dc463c4657f807d5
Adrian Czyrek <adrianczyrek@MacBook-Air-Adrian.local>
2023-11-20 20:24:18 +0100

    Fixes JS Data

--- game.js
+++ game.js
@@ -7,72 +7,73 @@ let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
 let questions = [
   {
-    question: "Who is the best basketball player?",
+    question: "What is the best basketball player in the world??",
     choice1: "Lebron James",
     choice2: "Michael Jordan",
     choice3: "Kobe Bryant",
-    choice4: "Kevin Durant",
-    answer: 1,
+    choice4: "Stephen Curry",
+    answer: 1
     
   },
   {
-    question: "What is the most popular food in Italy?",
+    question:
+      "What is the most famous food in Italy?",
     choice1: "Pizza",
     choice2: "Pasta",
-    choice3: "Spaghetti",
-    choice4: "Lasagna",
-    answer: 2,
+    choice3: "Lasagna",
+    choice4: "Spaghetti",
+    answer: 2
     
   },
   {
-    question: "What is the biggest country in Europe?",
+    question: " What is the biggest country in Europe?",
     choice1: "Germany",
-    choice2: "Russia",
-    choice3: "France",
-    choice4: "Italy",
-    answer: 2,
+    choice2: "France",
+    choice3: "Italy",
+    choice4: "Russia",
+    answer: 4
     
-  },
+  }
 ];
 
-// Constants
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
-  availableQuestions = [...questions];
+  availableQuesions = [...questions];
   getNewQuestion();
 };
 
 getNewQuestion = () => {
-  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    // Go to the end page
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
     return window.location.assign("/end.html");
   }
-
   questionCounter++;
   questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
   question.innerText = currentQuestion.question;
 
-  choices.forEach((choice) => {
+  choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
 
-  availableQuestions.splice(questionIndex, 1);
+  availableQuesions.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
 
-choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
@@ -80,22 +81,22 @@ choices.forEach((choice) => {
     const selectedAnswer = selectedChoice.dataset["number"];
 
     const classToApply =
-      selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
     }
 
     selectedChoice.parentElement.classList.add(classToApply);
+
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
     }, 1000);
-
-    getNewQuestion();
   });
 });
 
-incrementScore = (num) => {
+incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
